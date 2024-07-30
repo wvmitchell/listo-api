89fc521f8ad708273af801a9a8955750b7a4d0f0
Will Mitchell <wvmitchell@gmail.com>
2024-07-30 13:46:23 -0400

    wip add tests to handlers

---
+++ routeHandlers/routehandlers_test.go
@@ -0,0 +1,181 @@
+// Package routehandlers provides the route handlers for the application.
+package routehandlers
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Test_getUserID(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getUserID(tt.args.c); got != tt.want {
+				t.Errorf("getUserID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetChecklists(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetChecklists(tt.args.c)
+		})
+	}
+}
+
+func TestGetChecklist(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetChecklist(tt.args.c)
+		})
+	}
+}
+
+func TestPutChecklist(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PutChecklist(tt.args.c)
+		})
+	}
+}
+
+func TestPostChecklist(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PostChecklist(tt.args.c)
+		})
+	}
+}
+
+func TestDeleteChecklist(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DeleteChecklist(tt.args.c)
+		})
+	}
+}
+
+func TestPostItem(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PostItem(tt.args.c)
+		})
+	}
+}
+
+func TestPutItem(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PutItem(tt.args.c)
+		})
+	}
+}
+
+func TestPutAllItems(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			PutAllItems(tt.args.c)
+		})
+	}
+}
+
+func TestDeleteItem(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DeleteItem(tt.args.c)
+		})
+	}
+}
---
+++ test/routehandler.test.go
@@ -0,0 +1,4 @@
+// test the routehandlers.go file
+
+// Package routehandlers provides the functions to handle the routes.
+package routehandlers
