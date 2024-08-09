06c67930633fdb10ae279a93f1cbe5ab4b94f582
Will Mitchell <wvmitchell@gmail.com>
2024-08-09 17:47:26 -0400

    include collaborators on checklist model, minus the id

--- models/models.go
+++ models/models.go
@@ -3,12 +3,12 @@ package models
 
 // Checklist is a collection of ChecklistItems.
 type Checklist struct {
-	ID            string   `json:"id"`
-	Title         string   `json:"title"`
-	Locked        bool     `json:"locked"`
-	Collaborators []string `json:"collaborators"`
-	CreatedAt     string   `json:"created_at"`
-	UpdatedAt     string   `json:"updated_at"`
+	ID            string         `json:"id"`
+	Title         string         `json:"title"`
+	Locked        bool           `json:"locked"`
+	Collaborators []Collaborator `json:"collaborators"`
+	CreatedAt     string         `json:"created_at"`
+	UpdatedAt     string         `json:"updated_at"`
 }
 
 // ChecklistItem is a single item in a checklist.
@@ -24,6 +24,12 @@ type ChecklistItem struct {
 // User is a user of the application. Most info is actually stored in Auth0.
 type User struct {
 	ID      string `json:"id"`
+	Email   string `json:"email"`
+	Picture string `json:"picture"`
+}
+
+// Collaborator is a user that has been invited to collaborate on a checklist. The ID is removed for extra security, and becuase it's not needed on the client.
+type Collaborator struct {
 	Email   string `json:"email"`
 	Picture string `json:"picture"`
 }
